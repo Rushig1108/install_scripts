2d276b2940f20d6289f5ab2c828f6699a67daa26
Rushig1108 <rushigunjal17@gmail.com>
2025-03-09 19:07:52 -0400

    added a sonarqube installtion script

---
+++ sonar_qube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo su 
+adduser sonarqube 
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip
+unzip *
+chmod -R 755 /home/sonarqube/sonarqube-9.4.0.54424
+chown -R sonarqube:sonarqube /home/sonarqube/sonarqube-9.4.0.54424
+cd sonarqube-9.4.0.54424/bin/linux-x86-64/
+sudo su sonarqube
+./sonar.sh start
